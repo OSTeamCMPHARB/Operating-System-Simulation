1c2ebcec3fe67ce33b4095554054173ffa4ba3b6
ahmed-8areeb <62256670+ahmed-8areeb@users.noreply.github.com>
2021-06-02 21:42:21 +0200

    updated memory after adding Best

--- memory.c
+++ memory.c
@@ -13,11 +13,9 @@ int main(int agrc, char *argv[])
         memory[i] = false;
     }
 
-
     int msgq_id_SM = getID_SM();
     int msgq_id_MS = getID_MS();
 
-    
     int val;
 
     memoBuff message;
@@ -32,17 +30,16 @@ int main(int agrc, char *argv[])
             {
                 if (message.mtype == 1)
                 {
-                    break;//terminate program is finished
+                    break; //terminate program is finished
                 }
                 else if (message.mtype == 2)
-                {//deallocate the following memory
+                { //deallocate the following memory
                     for (int i = message.m.start; i < message.m.memorySize + message.m.start; i++)
                     {
                         memory[i] = false;
                     }
-
                 }
-                else if(message.mtype==3)
+                else if (message.mtype == 3)
                 {
                     //printf("computing in memo \n");
                     int counter = 0;
@@ -59,7 +56,7 @@ int main(int agrc, char *argv[])
                                 counter++;
                                 i++;
                                 if (counter >= message.m.memorySize)
-                                {                      //checking if we found a space for process
+                                {                   //checking if we found a space for process
                                     spaceFound = 1; //will make me exit both loops & send to scheduler that the memory needed is available
                                     break;
                                 }
@@ -73,7 +70,7 @@ int main(int agrc, char *argv[])
                     {
                         printf("finished computing \n");
                         message.mtype = 1; //used as true in case the space is found
-                        message.m.start=start;
+                        message.m.start = start;
                         //allocate the space found
                         for (int j = start; j < message.m.memorySize + start; j++)
                         {
@@ -82,32 +79,33 @@ int main(int agrc, char *argv[])
                     }
                     else
                     {
-                        message.m.start=-1;
+                        message.m.start = -1;
                     }
                     //printf("sending permission \n");
-                    
+
                     val = msgsnd(msgq_id_MS, &message, sizeof(message) - sizeof(long), !IPC_NOWAIT);
-                    spaceFound=0;
+                    spaceFound = 0;
                 }
             }
         }
     }
 
     else if (memoAlgorithm == 2)
-    {//Next fit Algorithm
+    { //Next fit Algorithm
         printf("hello from next fit algorithm \n");
         int last_filled_bit = -1;
         int empty_bytes = 1024;
-        while(true)
+        while (true)
         {
             val = msgrcv(msgq_id_SM, &message, sizeof(message) - sizeof(long), 0, !IPC_NOWAIT);
-            if(val != -1){
+            if (val != -1)
+            {
                 if (message.mtype == 1)
                 {
                     printf("number of full bytes = %d \n", empty_bytes);
                     break;
                 }
-                else if(message.mtype == 2)
+                else if (message.mtype == 2)
                 {
                     for (int i = message.m.start; i < message.m.memorySize + message.m.start; i++)
                     {
@@ -116,22 +114,23 @@ int main(int agrc, char *argv[])
                     }
                     printf("number of empty bytes = %d \n", empty_bytes);
                 }
-                else if(message.mtype == 3)
+                else if (message.mtype == 3)
                 {
                     int begin_search_index = last_filled_bit;
                     last_filled_bit++;
                     int start_memory = last_filled_bit;
                     int free_space_counter = 0;
                     bool found_enough_space = false;
-                    while(last_filled_bit != begin_search_index && !found_enough_space)
+                    while (last_filled_bit != begin_search_index && !found_enough_space)
                     {
-                        if(last_filled_bit == 1024) 
+                        if (last_filled_bit == 1024)
                         {
                             last_filled_bit = 0;
                             start_memory = 0;
                             free_space_counter = 0;
                         }
-                        if(memory[last_filled_bit]){
+                        if (memory[last_filled_bit])
+                        {
                             last_filled_bit++;
                             start_memory = last_filled_bit;
                             free_space_counter = 0;
@@ -139,18 +138,20 @@ int main(int agrc, char *argv[])
                         }
                         free_space_counter++;
                         last_filled_bit++;
-                        if(free_space_counter == message.m.memorySize - 1){
-                            found_enough_space =  true;
+                        if (free_space_counter == message.m.memorySize - 1)
+                        {
+                            found_enough_space = true;
                             printf("the last process ends at %d \n", last_filled_bit);
-                        } 
+                        }
                     }
-                    if(found_enough_space)
+                    if (found_enough_space)
                     {
-                        printf("space found the process memory start at index %d and end at index %d and has space = %d\n",start_memory, last_filled_bit, free_space_counter);
+                        printf("space found the process memory start at index %d and end at index %d and has space = %d\n", start_memory, last_filled_bit, free_space_counter);
                         printf("finished computing \n");
                         message.mtype = 1; //used as true in case the space is found
                         message.m.start = start_memory;
-                        for(int i = start_memory; i <= free_space_counter + start_memory; i++){
+                        for (int i = start_memory; i <= free_space_counter + start_memory; i++)
+                        {
                             memory[i] = true;
                             empty_bytes--;
                         }
@@ -168,6 +169,69 @@ int main(int agrc, char *argv[])
     //////////////////////////////////////end trial///////////////////////
     else if (memoAlgorithm == 3)
     {
+
+        while (true)
+        {
+            val = msgrcv(msgq_id_SM, &message, sizeof(message) - sizeof(long), 0, !IPC_NOWAIT); //recieving the request of memory allocation 3=allocate / 1=terminate / 2=deallocate
+            if (val != 0)
+            {
+                if (message.mtype == 1)
+                {
+                   // printf("%d",x);
+                    break; //terminate program is finished
+                }
+                else if (message.mtype == 2)
+                { //deallocate the following memory
+                
+                    for (int i = message.m.start; i < message.m.memorySize + message.m.start; i++)
+                    {
+                        memory[i] = false;
+                    }
+                }
+                else if (message.mtype == 3)
+                {
+                    //printf("computing in memo \n");
+                    int counter = 0;
+                    int i = 0;
+                    int start = 0;
+                    int min = 5000;
+                    int temp;
+                    while (i < 1024)
+                    {
+                        temp = 0;
+                        counter = i;
+                        while (i < 1024 && !memory[i])
+                        {
+                            temp++;
+                            i++;
+                        }
+                        if (temp >= message.m.memorySize && temp < min)//1023
+                        {
+                            start = counter;
+                            min = temp;
+                        }
+                        i++;
+                    }
+                    if (min != 5000)
+                    {
+                        printf("finished computing \n");
+                        message.mtype = 1; //used as true in case the space is found
+                        message.m.start = start;
+                        //allocate the space found
+                        
+                        for (int j = start; j < message.m.memorySize + start; j++)
+                        {
+                            memory[j] = true;
+                        }
+                    }
+                    else
+                    {
+                        message.m.start = -1;
+                    }
+                    val = msgsnd(msgq_id_MS, &message, sizeof(message) - sizeof(long), !IPC_NOWAIT);
+                }
+            }
+        }
     }
     else if (memoAlgorithm == 4)
     {
